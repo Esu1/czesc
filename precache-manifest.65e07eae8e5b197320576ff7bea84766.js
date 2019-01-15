5ae4d9a575c7d5a571789e899ca6bb27f0269b9e
Michał <august9@poczta.onet.pl>
2019-01-15 23:14:12 +0100

    Updates

---
+++ precache-manifest.65e07eae8e5b197320576ff7bea84766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/czesc/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "078416b2fd1ef5330718",
+    "url": "/czesc/static/js/runtime~main.078416b2.js"
+  },
+  {
+    "revision": "6047f603cd4adba91d2a",
+    "url": "/czesc/static/js/main.6047f603.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/czesc/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "6047f603cd4adba91d2a",
+    "url": "/czesc/static/css/main.e42b7fa5.chunk.css"
+  },
+  {
+    "revision": "a0c5362d87eb8ad00f1e24e2d18063a7",
+    "url": "/czesc/index.html"
+  }
+];
